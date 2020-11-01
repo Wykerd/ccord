8f1f78ccb302a2b39ae8943e85e0b12c06ed236f
Daniel Wykerd <danielwykerd@gmail.com>
2020-11-01 22:02:16 +0200

    Refactor

---
+++ src/test.c
@@ -0,0 +1,49 @@
+#include "http.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int on_body_recv (llhttp_t* parser, const char *at, size_t length) {
+    fwrite(at, length, sizeof(char), stdout);
+
+    return 0;
+}
+
+const char* request = "GET / HTTP/1.1\r\nHost: www.google.com\r\nUser-Agent: bazcal/3.0\r\nConnection: close\r\n\r\n";
+
+void write_cb (fa_http_client_t *client, fa_http_client_err_t *error) {
+    free(client->data);
+    puts("WROTE DATA");
+}
+
+void client_connect_cb (fa_http_client_t *client, fa_http_client_err_t *err) {
+    if (err != NULL) printf("Status %d\n", err->code);
+    else puts("Connected");
+
+    uv_buf_t *resbuf = malloc(sizeof(uv_buf_t));
+    
+    resbuf->base = (char *)request;
+    resbuf->len = strlen(request);
+
+    client->data = resbuf;
+
+    fa_http_client_write (client, resbuf, *write_cb);
+}
+
+int main () {
+    gnutls_global_init();
+
+    const char* url_raw = "https://www.google.com";
+
+    uv_loop_t *loop = uv_default_loop();
+
+    fa_http_client_t client;
+
+    fa_http_client_init(loop, &client);
+
+    client.parser_settings.on_body = *on_body_recv;
+
+    fa_http_client_connect (&client, url_raw, *client_connect_cb);
+
+    uv_run(loop, UV_RUN_DEFAULT);
+}
